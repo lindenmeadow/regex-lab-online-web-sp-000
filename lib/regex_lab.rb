3a799c14cbb35996dd1807bd0e40acdf142981b7
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-05-30 02:09:35 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,35 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if text.scan()
+    return true
+  else
+    return false
+  end
 end
 
 def words_five_letters_long(text)
-
+  if text.scan(/\b[a-zA-z]{5}\b/)
+    return true
+  else
+    return false
+  end
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
 
-end
+
 
 def valid_phone_number?(phone)
-
+  if phone.scan(/\d{10}/)
+    return true
+  else
+    return false
+  end
 end
