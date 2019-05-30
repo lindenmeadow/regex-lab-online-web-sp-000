5eafb795b87537eaab847bf59280aed916928225
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-05-30 02:11:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  if text.scan(/\b[a-zA-z]{5}\b/)
+  if text.scan(/\b\w{5}\b/)
     return true
   else
     return false
