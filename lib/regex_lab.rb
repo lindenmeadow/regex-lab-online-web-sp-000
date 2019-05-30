535e9ca5c2c937c60e408b02162e21ab80f53a01
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-05-30 02:14:59 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,27 +7,23 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.scan()
-    return true
-  else
-    return false
-  end
+  text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-  if text.scan(/\b[a-zA-z]{5}\b/)
+  text.scan(/\b\w{5}\b/)
+end
+
+def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.match(/^[A-Z].+[\.!?]$/)
     return true
   else
     return false
   end
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
-
-
 def valid_phone_number?(phone)
-  if phone.scan(/\d{10}/)
+  if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
     return true
   else
     return false
